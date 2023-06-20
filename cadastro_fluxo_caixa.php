80c8e902be51f45a44928916d05f344a0b4a20b0
MatheusI <matheusinoue2006@gmail.com>
2023-06-20 08:41:27 -0300

    prova2

---
+++ cadastro_fluxo_caixa.php
@@ -0,0 +1,33 @@
+<?php
+    include("conexao.php");
+
+    
+    $data = date("Y/m/d");
+    $tipo = $_POST['tipo'];
+    $valor = $_POST['valor'];
+    $historico = $_POST['historico'];
+    $cheque = $_POST['cheque'];
+
+    echo "<h1>Dados do usuário</h1>";
+    echo "Data: $data <br>";
+    echo "Tipo: $tipo <br>";
+    echo "Valor: $valor <br>";
+    echo "Historico: $historico <br>";
+    echo "Cheque: $cheque <br>";
+    
+
+    $sql = "INSERT INTO agenda (data, tipo, valor, historico, cheque)";
+    
+
+    $sql .= " VALUES ('".$data."','".$tipo."','".$valor."',
+                         '".$historico."','".$cheque."')";
+
+    echo $sql."<br>";
+    $result = mysqli_query($con, $sql);
+
+    if ($result)
+        echo "Dados cadastrados com sucesso!";
+    else
+        echo "Erro ao tentar cadastrar!";
+        
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Pagina Inicial - Fluxo de caixa</h3>
+    <hr>
+    <ul>
+        <li><a href='cadastro_fluxo_caixa.html'>Cadastro Fluxo de Caixa</a></li> 
+        <li><a href='listar_fluxo_de_caixa.php'>Listar Fluxo de Caixa</a></li> 
+        <li><a href='consulta_saldo_de_caixa.php'>Consultar Saldo de Caixa</a></li> 
+    </ul>
+</body>
+</html>
